5546df627755a2a38e9042c42ef548ecb7cb069f
sathishkumar330 <svksathish41@gmail.com>
2023-03-24 16:05:31 +0530

    add

---
+++ main.js
@@ -0,0 +1,11 @@
+let a=new Date();
+var b=(a.getMonth());
+	if(b>=0 && b<=3){
+		console.log("summer");
+}
+	else if(b>=4 && b<=7){
+		console.log("winter");
+}
+	else if(b>=8 && b<=11){
+		console.log("fall");
+}
